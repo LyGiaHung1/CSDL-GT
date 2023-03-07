a7a9cedf9b4bdd0a5fe117b44ef5ca07b09ab964
LyGiaHung1 <124952239+LyGiaHung1@users.noreply.github.com>
2023-03-07 19:11:55 +0700

    Tổng các số chẵn trong mảng

---
+++ Bài tập/Tongchan.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<iostream>
+#define Max 100
+using namespace std;
+void Nhap(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap vao a[" << i << "]:";
+		cin >> a[i];
+	}
+}
+void Xuat(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << "" <<endl;
+	}
+}
+int Tong(int a[], int n)
+{
+	if (n == 0)
+		return 0;
+	int s = Tong(a, n - 1);
+	if (a[n - 1] % 2 == 0)
+		s = s + a[n - 1];
+	return s;
+}
+
+int main()
+{
+	int a[Max];
+	int n;
+	cout << "Nhap n:";
+	cin >> n;
+	Nhap(a, n);	
+	Xuat(a, n);
+	int tong = Tong(a, n);
+	cout <<"Tong cac so chan:" << tong;
+	return 0;
+}
